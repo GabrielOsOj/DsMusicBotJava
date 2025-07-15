8d1adf78e7c6ed72df212243168f6a5b4484f7e9
GabrielOsOj <102068755+GabrielOsOj@users.noreply.github.com>
2025-07-15 01:31:54 -0300

    Fix: cambiando metodo de inicio

--- src/main/java/DiscordJavaBotV2.java
+++ src/main/java/DiscordJavaBotV2.java
@@ -9,18 +9,18 @@ public class DiscordJavaBotV2 {
 
 	public static void main(String[] args) {
 
-		// Scanner scanner = new Scanner(System.in);
-		// System.out.println("Paste you discord key to start the bot");
-		// String key = scanner.next();
-		// BotManager bot = new BotManager();
-		//
-		// bot.run(key);
+		 Scanner scanner = new Scanner(System.in);
+		 System.out.println("Paste you discord key to start the bot");
+		 String key = scanner.next();
+		 BotManager bot = new BotManager();
+		
+		 bot.run(key);
 
 		/* TEST */
 
-		Constantes cts = new Constantes();
-		BotManager bot = new BotManager();
-		bot.run(cts.getKey("DISCORD_BOT_TOKEN"));
+//		Constantes cts = new Constantes();
+//		BotManager bot = new BotManager();
+//		bot.run(cts.getKey("DISCORD_BOT_TOKEN"));
 
 		// SongManager sg = new SongManager();
 		//
